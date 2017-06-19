f476fa14089e24294c4c28f1fcbdbcc4a6284e5c
mishu <mishu@xtelecom.ro>
2017-06-19 17:52:10 +0300

    updated structure creator with default attribute for permissions

--- src/database/migrations/2017_01_01_148000_create_structure_for_contact_persons.php
+++ src/database/migrations/2017_01_01_148000_create_structure_for_contact_persons.php
@@ -9,14 +9,14 @@ class CreateStructureForContactPersons extends StructureMigration
     ];
 
     protected $permissions = [
-        ['name' => 'administration.contactPersons.index', 'description' => 'Contact Persons index', 'type' => 0],
-        ['name' => 'administration.contactPersons.edit', 'description' => 'Edit Contact Person', 'type' => 0],
-        ['name' => 'administration.contactPersons.create', 'description' => 'Create Contact Person', 'type' => 0],
-        ['name' => 'administration.contactPersons.update', 'description' => 'Update Contact Person', 'type' => 1],
-        ['name' => 'administration.contactPersons.store', 'description' => 'Store Contact Person', 'type' => 1],
-        ['name' => 'administration.contactPersons.destroy', 'description' => 'Delete Contact Person', 'type' => 1],
-        ['name' => 'administration.contactPersons.initTable', 'description' => 'Init Table for Contact Person', 'type' => 0],
-        ['name' => 'administration.contactPersons.getTableData', 'description' => 'Table Data for Contact Person', 'type' => 0],
+        ['name' => 'administration.contactPersons.index', 'description' => 'Contact Persons index', 'type' => 0, 'default' => false],
+        ['name' => 'administration.contactPersons.edit', 'description' => 'Edit Contact Person', 'type' => 0, 'default' => false],
+        ['name' => 'administration.contactPersons.create', 'description' => 'Create Contact Person', 'type' => 0, 'default' => false],
+        ['name' => 'administration.contactPersons.update', 'description' => 'Update Contact Person', 'type' => 1, 'default' => false],
+        ['name' => 'administration.contactPersons.store', 'description' => 'Store Contact Person', 'type' => 1, 'default' => false],
+        ['name' => 'administration.contactPersons.destroy', 'description' => 'Delete Contact Person', 'type' => 1, 'default' => false],
+        ['name' => 'administration.contactPersons.initTable', 'description' => 'Init Table for Contact Person', 'type' => 0, 'default' => false],
+        ['name' => 'administration.contactPersons.getTableData', 'description' => 'Table Data for Contact Person', 'type' => 0, 'default' => false],
     ];
 
     protected $menu = [
