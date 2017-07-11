8cca7508fa6fe3fc9745f00c73388b984cc98712
mishu <mishu@xtelecom.ro>
2017-07-11 17:44:36 +0300

    added exportExcel route

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,6 +7,7 @@ Route::group([
     Route::group(['prefix' => 'administration/contactPersons', 'as' => 'administration.contactPersons.'], function () {
         Route::get('initTable', 'ContactPersonController@initTable')->name('initTable');
         Route::get('getTableData', 'ContactPersonController@getTableData')->name('getTableData');
+        Route::get('exportExcel', 'ContactPersonController@exportExcel')->name('exportExcel');
     });
 
     Route::group(['prefix' => 'administration', 'as' => 'administration.'], function () {
