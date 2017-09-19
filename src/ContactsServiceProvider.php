e519b6a48f5ac2041d6a0e24b4cb8016ad76ce13
Mihai <mihai.ocneanu@gmail.com>
2017-09-19 14:25:03 +0300

    fixed versions. Moved VueJS components to the vueadminlte package

--- src/ContactsServiceProvider.php
+++ src/ContactsServiceProvider.php
@@ -19,14 +19,6 @@ class ContactsServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/config' => config_path(),
         ], 'enso-config');
-
-        $this->publishes([
-            __DIR__.'/resources/assets/js/components' => resource_path('assets/js/vendor/laravel-enso/components'),
-        ], 'contacts-component');
-
-        $this->publishes([
-            __DIR__.'/resources/assets/js/components' => resource_path('assets/js/vendor/laravel-enso/components'),
-        ], 'enso-update');
     }
 
     public function register()
