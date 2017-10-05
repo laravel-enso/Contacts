36c4046f7d13ebda3901c213ca4004b06cc09232
mishu <mishu@xtelecom.ro>
2017-10-05 14:53:53 +0300

    refactor routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,24 +1,22 @@
 <?php
 
-Route::middleware(['auth:api', 'api', 'core'])
-    ->prefix('api')
+Route::middleware(['web', 'auth', 'core'])
+    ->prefix('api/core')->as('core.')
     ->namespace('LaravelEnso\Contacts\app\Http\Controllers')
     ->group(function () {
-        Route::prefix('core')->as('core.')
+        Route::prefix('contacts')->as('contacts.')
             ->group(function () {
-                Route::prefix('contacts')->as('contacts.')
-                    ->group(function () {
-                        Route::get('initTable', 'ContactTableController@initTable')
-                            ->name('initTable');
-                        Route::get('getTableData', 'ContactTableController@getTableData')
-                            ->name('getTableData');
-                        Route::get('exportExcel', 'ContactTableController@exportExcel')
-                            ->name('exportExcel');
+                Route::get('initTable', 'ContactTableController@initTable')
+                    ->name('initTable');
+                Route::get('getTableData', 'ContactTableController@getTableData')
+                    ->name('getTableData');
+                Route::get('exportExcel', 'ContactTableController@exportExcel')
+                    ->name('exportExcel');
 
-                        Route::get('list', 'ContactController@list')
-                            ->name('list');
-                    });
-
-                Route::resource('contacts', 'ContactController', ['exclude' => ['show', 'create', 'edit']]);
+                Route::get('list', 'ContactController@list')
+                    ->name('list');
             });
+
+        Route::resource('contacts', 'ContactController', ['exclude' => ['show', 'create', 'edit']]);
+
     });
