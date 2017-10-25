2bf306a08190e9b753f6b9dec32b44db6fb2120e
mishu <mishu@xtelecom.ro>
2017-10-25 13:34:35 +0300

    updates validatios rules

--- src/app/Http/Requests/ValidateContactRequest.php
+++ src/app/Http/Requests/ValidateContactRequest.php
@@ -3,7 +3,6 @@
 namespace LaravelEnso\Contacts\app\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class ValidateContactRequest extends FormRequest
 {
@@ -14,19 +13,11 @@ class ValidateContactRequest extends FormRequest
 
     public function rules()
     {
-        $emailUnique = Rule::unique('contacts', 'email');
-        $phoneUnique = Rule::unique('contacts', 'phone');
-
-        if (request()->getMethod() == 'PATCH') {
-            $emailUnique = $emailUnique->ignore(request()->get('contact')['id']);
-            $phoneUnique = $phoneUnique->ignore(request()->get('contact')['id']);
-        }
-
         return [
             'first_name' => 'required|max:50',
             'last_name'  => 'required|max:50',
-            'email'      => ['email', 'nullable', $emailUnique],
-            'phone'      => ['nullable', $phoneUnique],
+            'email'      => 'email|nullable',
+            'phone'      => 'nullable',
         ];
     }
 }
