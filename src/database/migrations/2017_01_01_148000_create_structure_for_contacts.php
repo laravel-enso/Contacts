df5bc36444f71b3a623e058e7343fc013b8ff935
mishu <mishu@xtelecom.ro>
2018-05-28 13:21:24 +0300

    updates for the new structure manager

--- src/database/migrations/2017_01_01_148000_create_structure_for_contacts.php
+++ src/database/migrations/2017_01_01_148000_create_structure_for_contacts.php
@@ -5,15 +5,15 @@ use LaravelEnso\StructureManager\app\Classes\StructureMigration;
 class CreateStructureForContacts extends StructureMigration
 {
     protected $permissions = [
-        ['name' => 'core.contacts.index', 'description' => 'Contacts index', 'type' => 0, 'default' => false],
-        ['name' => 'core.contacts.create', 'description' => 'Get create form  for contacts', 'type' => 0, 'default' => false],
-        ['name' => 'core.contacts.edit', 'description' => 'Get edit form for contacts', 'type' => 0, 'default' => false],
-        ['name' => 'core.contacts.update', 'description' => 'Update edited contact', 'type' => 1, 'default' => false],
-        ['name' => 'core.contacts.store', 'description' => 'Store newly created contact', 'type' => 1, 'default' => false],
-        ['name' => 'core.contacts.destroy', 'description' => 'Delete contact', 'type' => 1, 'default' => false],
-        ['name' => 'core.contacts.initTable', 'description' => 'Init table for contacts', 'type' => 0, 'default' => false],
-        ['name' => 'core.contacts.getTableData', 'description' => 'Get table data for contacts', 'type' => 0, 'default' => false],
-        ['name' => 'core.contacts.exportExcel', 'description' => 'Export excel for contacts', 'type' => 0, 'default' => false],
+        ['name' => 'core.contacts.index', 'description' => 'Contacts index', 'type' => 0, 'is_default' => false],
+        ['name' => 'core.contacts.create', 'description' => 'Get create form  for contacts', 'type' => 0, 'is_default' => false],
+        ['name' => 'core.contacts.edit', 'description' => 'Get edit form for contacts', 'type' => 0, 'is_default' => false],
+        ['name' => 'core.contacts.update', 'description' => 'Update edited contact', 'type' => 1, 'is_default' => false],
+        ['name' => 'core.contacts.store', 'description' => 'Store newly created contact', 'type' => 1, 'is_default' => false],
+        ['name' => 'core.contacts.destroy', 'description' => 'Delete contact', 'type' => 1, 'is_default' => false],
+        ['name' => 'core.contacts.initTable', 'description' => 'Init table for contacts', 'type' => 0, 'is_default' => false],
+        ['name' => 'core.contacts.getTableData', 'description' => 'Get table data for contacts', 'type' => 0, 'is_default' => false],
+        ['name' => 'core.contacts.exportExcel', 'description' => 'Export excel for contacts', 'type' => 0, 'is_default' => false],
     ];
 
     protected $permissionGroup = [
@@ -21,7 +21,7 @@ class CreateStructureForContacts extends StructureMigration
     ];
 
     protected $menu = [
-        'name' => 'Contacts', 'icon' => 'address-book', 'link' => 'core.contacts.index', 'order' => 999, 'has_children' => false,
+        'name' => 'Contacts', 'icon' => 'address-book', 'link' => 'core.contacts.index', 'order_index' => 999, 'has_children' => false,
     ];
 
     protected $parentMenu = 'Administration';
