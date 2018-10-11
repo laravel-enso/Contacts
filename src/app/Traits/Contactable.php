0b6508967da555336ca27f4ef2c3926005c0c2fa
mishu <mishu@xtelecom.ro>
2018-10-11 08:27:23 +0300

    adds `onDelete` config option / improves `Contactable` trait

--- src/app/Traits/Contactable.php
+++ src/app/Traits/Contactable.php
@@ -3,9 +3,28 @@
 namespace LaravelEnso\Contacts\app\Traits;
 
 use LaravelEnso\Contacts\app\Models\Contact;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 
 trait Contactable
 {
+    public static function bootContactable()
+    {
+        self::deleting(function ($model) {
+            if (config('enso.contacts.onDelete') === 'restrict'
+                && $model->contacts()->first() !== null) {
+                throw new ConflictHttpException(
+                    __('The entity has contacts and cannot be deleted')
+                );
+            }
+        });
+
+        self::deleted(function ($model) {
+            if (config('enso.contacts.onDelete') === 'cascade') {
+                $model->contacts()->delete();
+            }
+        });
+    }
+
     public function contacts()
     {
         return $this->morphMany(Contact::class, 'contactable');
